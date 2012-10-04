a4f335ae14e66956eeff7de3b2886793ac9da967
mrtseb <mrtseb@b096a3e4-f5a8-a225-c8b2-2342011f3706>
2012-10-04 17:55:42 +0000

    depot1

---
+++ trunk/sinatra/public/js/init.js
@@ -0,0 +1,11 @@
+jQuery.prototype.highlight = function() {
+  jQuery(this).css("background-color","yellow").fadeTo('slow', 0.1, function() {
+    jQuery(this).fadeTo('slow', 1.0, function() {
+        jQuery(this).css("background-color","yellow").fadeTo('slow', 0, function() {});
+    });
+  });
+};
+
+$(function() {
+	jQuery('#flash').highlight();
+});
